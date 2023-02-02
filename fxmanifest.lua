5f8ec6305ca2ce71344eec22ee5fa4945400f210
Guudz <Kilian1702@outlook.com>
2023-02-02 01:37:44 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author "! ZeroK#0001"
+description "Lidl map for FiveM"
+version "1.0"
+
+server_scripts {
+	'server/server.lua',
+}
+
+files { "interiorproxies.meta" }
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
